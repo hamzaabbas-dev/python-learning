bc11dae7ba04180e801b206f477f45b0ffa86422
Hamza Abbas <hamza.abbas.devops@gmail.com>
2026-09-16 10:31:00 +0500

    Complete Python Chapter 06 practice

---
+++ CHAPTER_06/02_problem.py
@@ -0,0 +1,4 @@
+l = ["Hamza", "Umer" , "Hania", "Rimsha"]
+for name in l:
+    if (name.startswith("H") or name.startswith("R")):
+        print(f"Hello ! {name}")   
---
+++ CHAPTER_06/06_problem.py
@@ -0,0 +1,5 @@
+numbers = [3, 7, 12, 15, 20]
+
+for number in numbers:
+    if number % 2 == 0:
+        print(number)
